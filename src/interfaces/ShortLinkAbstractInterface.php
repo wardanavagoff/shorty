ea8852e4d3843bbae43952ba79bbf4230f44bfa5
Mike Barwick <mike@barwickandco.com>
2016-11-07 16:05:54 -0500

    Fix

--- src/interfaces/ShortLinkAbstractInterface.php
+++ src/interfaces/ShortLinkAbstractInterface.php
@@ -25,31 +25,6 @@ abstract class ShortLinkAbstractInterface
     abstract public function expand($shortUrl);
 
     /**
-     * Validates if the URL to submit actually exists and is accessible.
-     *
-     * @param $url string                 URL being submitted.
-     * @throws \Sonrisa\Service\ShortLink\Exceptions\InvalidApiResponseException
-     * @return boolean
-     */
-    protected function urlCheck($url)
-    {
-        // first check if url has http:// prefix, if not, add it
-        $parsed = parse_url($url);
-
-        if (empty($parsed['scheme'])) {
-            $url = 'http://' . ltrim($url, '/');
-        }
-
-        $file_headers = get_headers($url);
-
-        if (!strpos($file_headers[0], "404 Not Found") > 0) {
-            return true;
-        }
-        
-        return false;
-    }
-
-    /**
      * Performs the Request to the API server.
      *
      * @param $url
